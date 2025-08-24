9550340e6319e191ae4c41c2def4ae3bb7407df0
Imran Hasan <imranhasanrimon5@gmail.com>
2025-08-25 00:19:54 +0600

    starting segment 4

--- segment-3/src/App.tsx
+++ segment-3/src/App.tsx
@@ -1,16 +1,10 @@
-import './App.css'
-import { Button } from './components/ui/button'
-import { decrement, increment } from './redux/features/counter/counterSlice';
-import { useAppDispatch, useAppSelector } from './redux/hooks'
+import ItemPage from "./home/items/ItemPage"
 
 function App() {
-  const value = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch()
+
   return (
-    <div className='flex items-center gap-5 justify-center'>
-      <Button onClick={() => dispatch(decrement())}>Decrement</Button>
-      <h2 className='text-5xl'>{value}</h2>
-      <Button onClick={() => dispatch(increment())}>Increment</Button>
+    <div className=''>
+      <ItemPage />
     </div>
   )
 }
---
+++ segment-3/src/home/items/data-table.tsx
@@ -0,0 +1,6 @@
+
+export default function DataTable() {
+    return (
+        <div> DataTable</div>
+    )
+}
